7b7ffa8fbcc4b9cfdbf20ffc7572d1192be4da9b
komcal <kom.sujautra@gmail.com>
2017-08-28 00:46:39 +0700

    onClick setState

--- src/containers/Content.js
+++ src/containers/Content.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
+  increase() {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+  decrease = () => {
+    this.setState({
+      count: this.state.count - 1,
+    })
+  }
   render() {
     return (
       <div className="App-content">
         <p>My name is {this.props.name}</p>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increase.bind(this)}>+</button>
+        <button onClick={this.decrease}>-</button>
       </div>
     );
   }
