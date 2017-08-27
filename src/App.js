f414954aeafc105063d0c1a2b28945f115c667f1
komcal <kom.sujautra@gmail.com>
2017-08-28 00:50:31 +0700

    setState input

--- src/App.js
+++ src/App.js
@@ -4,17 +4,11 @@ import Header from './containers/Header';
 import Content from './containers/Content';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "komcal"
-    }
-  }
   render() {
     return (
       <div className="App">
         <Header />
-        <Content name={this.state.name} />
+        <Content />
       </div>
     );
   }
--- src/containers/Content.js
+++ src/containers/Content.js
@@ -5,6 +5,7 @@ class Content extends Component {
     super(props);
     this.state = {
       count: 0,
+      name: 'no one.',
     }
   }
   increase() {
@@ -17,13 +18,24 @@ class Content extends Component {
       count: this.state.count - 1,
     })
   }
+  onInputChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      name: e.target.value,
+    });
+  }
   render() {
     return (
       <div className="App-content">
-        <p>My name is {this.props.name}</p>
+        <p>I'm {this.state.name}</p>
         <h1>{this.state.count}</h1>
         <button onClick={this.increase.bind(this)}>+</button>
         <button onClick={this.decrease}>-</button>
+        <input
+          type="text"
+          onChange={this.onInputChange}
+          value={this.state.name}
+        />
       </div>
     );
   }
